90abda13e5c7b065835ca547aa06f7a85a5ca4e3
Arya Krisna Putra <aryakrisna07@gmail.com>
2024-09-27 14:11:01 +0700

    first commit

---
+++ Assets/Scripts/Detector.cs
@@ -0,0 +1,237 @@
+﻿using Assets.Scripts;
+using Assets.Scripts.TextureProviders;
+using NN;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using Unity.Barracuda;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Profiling;
+using UnityEngine.UI;
+
+public class Detector : MonoBehaviour
+{
+    [Tooltip("File of YOLO model.")]
+    [SerializeField]
+    protected NNModel ModelFile;
+
+    [Tooltip("RawImage component which will be used to draw results.")]
+    [SerializeField]
+    protected RawImage ImageUI;
+
+    [Range(0.0f, 1f)]
+    [Tooltip("The minimum value of box confidence below which boxes won't be drawn.")]
+    [SerializeField]
+    protected float MinBoxConfidence = 0.3f;
+
+    [SerializeField]
+    protected TextureProviderType.ProviderType textureProviderType;
+
+    [SerializeReference]
+    protected TextureProvider textureProvider = null;
+
+    protected NNHandler nn;
+    protected Color[] colorArray = new Color[] { Color.red, Color.green, Color.blue, Color.cyan, Color.magenta, Color.yellow };
+
+    YOLOv8 yolo;
+
+    // Menggunakan array langsung untuk menyimpan nama kelas
+    private readonly string[] classNames = new string[]
+    {
+        "person",
+        "bicycle",
+        "car",
+        "motorcycle",
+        "airplane",
+        "bus",
+        "train",
+        "truck",
+        "boat",
+        "traffic light",
+        "fire hydrant",
+        "stop sign",
+        "parking meter",
+        "bench",
+        "bird",
+        "cat",
+        "dog",
+        "horse",
+        "sheep",
+        "cow",
+        "elephant",
+        "bear",
+        "zebra",
+        "giraffe",
+        "backpack",
+        "umbrella",
+        "handbag",
+        "tie",
+        "suitcase",
+        "frisbee",
+        "skis",
+        "snowboard",
+        "sports ball",
+        "kite",
+        "baseball bat",
+        "baseball glove",
+        "skateboard",
+        "surfboard",
+        "tennis racket",
+        "bottle",
+        "wine glass",
+        "cup",
+        "fork",
+        "knife",
+        "spoon",
+        "bowl",
+        "banana",
+        "apple",
+        "sandwich",
+        "orange",
+        "broccoli",
+        "carrot",
+        "hot dog",
+        "pizza",
+        "donut",
+        "cake",
+        "chair",
+        "couch",
+        "potted plant",
+        "bed",
+        "dining table",
+        "toilet",
+        "tv",
+        "laptop",
+        "mouse",
+        "remote",
+        "keyboard",
+        "cell phone",
+        "microwave",
+        "oven",
+        "toaster",
+        "sink",
+        "refrigerator",
+        "book",
+        "clock",
+        "vase",
+        "scissors",
+        "teddy bear",
+        "hair drier",
+        "toothbrush"
+    };
+
+    private List<(Rect, string)> boundingBoxes = new List<(Rect, string)>();
+
+    private void OnEnable()
+    {
+        nn = new NNHandler(ModelFile);
+        yolo = new YOLOv8Segmentation(nn);
+
+        textureProvider = GetTextureProvider(nn.model);
+        textureProvider.Start();
+    }
+
+    private void Update()
+    {
+        YOLOv8OutputReader.DiscardThreshold = MinBoxConfidence;
+        Texture2D texture = GetNextTexture();
+
+        var boxes = yolo.Run(texture);
+        DrawResults(boxes, texture);
+        ImageUI.texture = texture;
+    }
+
+    protected TextureProvider GetTextureProvider(Model model)
+    {
+        var firstInput = model.inputs[0];
+        int height = firstInput.shape[5];
+        int width = firstInput.shape[6];
+
+        TextureProvider provider;
+        switch (textureProviderType)
+        {
+            case TextureProviderType.ProviderType.WebCam:
+                provider = new WebCamTextureProvider(textureProvider as WebCamTextureProvider, width, height);
+                break;
+
+            case TextureProviderType.ProviderType.Video:
+                provider = new VideoTextureProvider(textureProvider as VideoTextureProvider, width, height);
+                break;
+            default:
+                throw new InvalidEnumArgumentException();
+        }
+        return provider;
+    }
+
+    protected Texture2D GetNextTexture()
+    {
+        return textureProvider.GetTexture();
+    }
+
+    void OnDisable()
+    {
+        nn.Dispose();
+        textureProvider.Stop();
+    }
+
+    protected void DrawResults(IEnumerable<ResultBox> results, Texture2D img)
+    {
+        boundingBoxes.Clear();  // Clear the list each frame
+        results.ForEach(box => 
+        {
+            DrawBox(box, img);
+            
+            // Check if the index is valid before accessing classNames
+            string className;
+            if (box.bestClassIndex >= 0 && box.bestClassIndex < classNames.Length)
+            {
+                className = classNames[box.bestClassIndex];
+            }
+            else
+            {
+                className = "Unknown"; // Handle invalid index
+                Debug.LogWarning($"Invalid class index: {box.bestClassIndex}");
+            }
+            
+            boundingBoxes.Add((box.rect, className));  // Store box rect and class name
+        });
+    }
+
+    protected virtual void DrawBox(ResultBox box, Texture2D img)
+    {
+        Color boxColor = colorArray[box.bestClassIndex % colorArray.Length];
+        int boxWidth = (int)(box.score / MinBoxConfidence);
+        TextureTools.DrawRectOutline(img, box.rect, boxColor, boxWidth, rectIsNormalized: false, revertY: true);
+    }
+
+    private void OnGUI()
+    {
+        foreach (var (rect, className) in boundingBoxes)
+        {
+            Vector2 labelPosition = new Vector2(rect.xMin, rect.yMin);
+            GUIStyle style = new GUIStyle();
+            style.normal.textColor = Color.white;
+            style.fontSize = 14;
+
+            GUI.Label(new Rect(labelPosition.x, Screen.height - labelPosition.y, 100, 20), className, style);
+        }
+    }
+
+    private void OnValidate()
+    {
+        Type t = TextureProviderType.GetProviderType(textureProviderType);
+        if (textureProvider == null || t != textureProvider.GetType())
+        {
+            if (nn == null)
+                textureProvider = RuntimeHelpers.GetUninitializedObject(t) as TextureProvider;
+            else
+            {
+                textureProvider = GetTextureProvider(nn.model);
+                textureProvider.Start();
+            }
+        }
+    }
+}
